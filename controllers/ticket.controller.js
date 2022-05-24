0bd94cd93004e0cf56385073e155f806955518bc
Rupam Kerketta <38970183+rupamkerketta@users.noreply.github.com>
2022-05-24 11:22:17 +0530

    feat(ticket): add initial update functionality

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -41,12 +41,82 @@ exports.createTicket = async (req, res) => {
 		engineer.ticketsAssigned.push(ticket._id)
 		await engineer.save()
 
-		return res
-			.status(StatusCodes.CREATED)
-			.send(objConverter.ticketResponse(ticket))
+		res.status(StatusCodes.CREATED).send(objConverter.ticketResponse(ticket))
 	} else {
 		// This thrown error will be seen as internal server error
 		// by the error handler
 		throw new Error()
 	}
+}
+
+// API to fetch all the tickets
+exports.getAllTickets = async (req, res) => {
+	const queryObj = { userId: req.userId }
+	const user = await User.findOne(queryObj)
+
+	if (user.ticketsCreated === null || user.ticketsCreated.length === 0) {
+		return res
+			.status(StatusCodes.OK)
+			.send({ message: 'No tickets created by you!' })
+	}
+
+	const ticketIds = user.ticketsCreated
+	const tickets = await Ticket.find({
+		_id: {
+			$in: ticketIds
+		}
+	})
+
+	res.status(StatusCodes.OK).send({ nbHits: tickets.length, tickets })
+}
+
+// API to fetch one ticket
+exports.getOneTicket = async (req, res) => {
+	const ticket = await Ticket.findOne({
+		_id: req.params.id
+	})
+
+	res.status(StatusCodes.OK).send(objConverter.ticketResponse(ticket))
+}
+
+// API to updated the ticket
+exports.updateTicket = async (req, res) => {
+	// Check of the ticket exists
+	const ticket = await Ticket.findOne({
+		_id: req.params.id
+	})
+
+	if (!ticket) {
+		return res
+			.status(StatusCodes.OK)
+			.send({ message: 'Ticket does not exist!' })
+	}
+
+	// Only the ticket requester be allowed to update the ticket
+	const user = await User.findOne({
+		userId: req.userId
+	})
+
+	if (!user.ticketsCreated.includes(req.params.id)) {
+		return res.status(StatusCodes.FORBIDDEN).send({
+			message: 'Only the owner of the ticket is allowed to update!'
+		})
+	}
+
+	// Update the attributes of the saved ticket
+	const { title, description, priority } = req.body
+
+	ticket.title = title === undefined ? ticket.title : title
+	ticket.description =
+		description === undefined ? ticket.description : description
+	ticket.priority = priority === undefined ? ticket.priority : priority
+
+	// Save the updated ticket
+	const updatedTicket = await ticket.save()
+
+	// Return the updated ticket
+	res.status(StatusCodes.OK).send({
+		message: `Ticket with id: ${ticket._id} updated successfully!`,
+		ticket: updatedTicket
+	})
 }
--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -4,6 +4,11 @@ const router = express.Router()
 const tickController = require('../controllers/ticket.controller')
 const authJwt = require('../middlewares/auth-jwt')
 
+router.route('/').get([authJwt.verifyToken], tickController.getAllTickets)
+router.route('/:id').get([authJwt.verifyToken], tickController.getOneTicket)
+
 router.route('/').post([authJwt.verifyToken], tickController.createTicket)
+
+router.route('/:id').patch([authJwt.verifyToken], tickController.updateTicket)
 
 module.exports = router
